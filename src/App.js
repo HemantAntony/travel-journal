bb6a5cd432f09b0674dbb984b92a4349e693c983
HemantAntony <hemantantony@outlook.com>
2024-07-18 19:01:54 +0530

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Header from "./components/Header"
 import Location from "./components/Location"
+import locationData from "./data"
 
 function App() {
+  const locations = locationData.map(item => {
+    return <Location {...item}/>
+  })
   return (
     <div className="App">
       <Header />
-      <Location />
+      <section className="cards">
+        {locations}
+      </section>
     </div>
   );
 }
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,19 +1,18 @@
-import image from "../images/japan.png"
 import point from "../images/point.png"
 
-export default function Location() {
+export default function Location({title, location, googleMapsUrl, startDate, endDate, description}) {
     return (
         <div className="location">
-            <img src={image} alt="location" className="location--image"></img>
+            <img src={`./images/${location}.png`} alt="location" className="location--image"></img>
             <div className="location--description">
                 <div className="location--place">
                     <img src={point} alt="point"></img>
-                    <span className="location--country">JAPAN</span>
-                    <a href="#" className="location--link">View on Google Maps</a>
+                    <span className="location--country">{location}</span>
+                    <a href={googleMapsUrl} className="location--link">View on Google Maps</a>
                 </div>
-                <h1 className="location--title">Mount Fuji</h1>
-                <h3 className="location--date">12 Jan, 2021 - 24 Jan, 2021</h3>
-                <p className="location--about">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h1 className="location--title">{title}</h1>
+                <h3 className="location--date">{`${startDate} - ${endDate}`}</h3>
+                <p className="location--about">{description}</p>
             </div>
         </div>
     )
